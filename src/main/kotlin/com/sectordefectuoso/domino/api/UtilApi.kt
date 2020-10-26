dec1837a4bccef111d5832850afcc2bc7928b532
autopsia <autopsia9@gmail.com>
2020-10-26 01:39:35 -0500

    Add cors filter. Map roles. Fix incomplete dependency.

--- src/main/kotlin/com/sectordefectuoso/domino/api/UtilApi.kt
+++ src/main/kotlin/com/sectordefectuoso/domino/api/UtilApi.kt
@@ -2,16 +2,24 @@ package com.sectordefectuoso.domino.api
 
 import com.fasterxml.jackson.databind.util.EnumValues
 import com.sectordefectuoso.domino.model.enum.QuestionTypeEnum
+import com.sectordefectuoso.domino.model.enum.RoleEnum
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
+@RequestMapping("/util")
 class UtilApi {
-    @GetMapping("/util")
+    @GetMapping("/question/types")
     fun getQuestionTypes(): Map<Int, String> {
         return QuestionTypeEnum.mapAll()
+    }
+
+    @GetMapping("/roles")
+    fun getRoles(): Map<Int, String> {
+        return RoleEnum.mapAll()
     }
 }
--- src/main/kotlin/com/sectordefectuoso/domino/model/enum/RoleEnum.kt
+++ src/main/kotlin/com/sectordefectuoso/domino/model/enum/RoleEnum.kt
@@ -1,7 +1,12 @@
 package com.sectordefectuoso.domino.model.enum
 
-enum class RoleEnum() {
-    STUDENT,
-    INSTRUCTOR,
-    ADMIN,
+enum class RoleEnum(val id: Int, val valueEs: String) {
+    ADMIN(1, "Administrador"),
+    STUDENT(2, "Estudiante"),
+    INSTRUCTOR(3, "Instructor");
+
+    companion object {
+        private val map = values().associate { it.id to it.valueEs }
+        fun mapAll() = map
+    }
 }
