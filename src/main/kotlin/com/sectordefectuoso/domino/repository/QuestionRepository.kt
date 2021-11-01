4e065ed00fea1298538941ff779626300a616df1
autopsia <autopsia9@gmail.com>
2021-10-31 19:49:14 -0500

    Add find random questions method.

--- src/main/kotlin/com/sectordefectuoso/domino/repository/QuestionRepository.kt
+++ src/main/kotlin/com/sectordefectuoso/domino/repository/QuestionRepository.kt
@@ -3,11 +3,17 @@ package com.sectordefectuoso.domino.repository
 import com.sectordefectuoso.domino.model.Question
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.aggregate
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.aggregation.Aggregation.*
 import org.springframework.data.mongodb.core.findOne
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Query.query
 import org.springframework.data.mongodb.core.query.where
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Repository
@@ -17,6 +23,14 @@ abstract class QuestionRepository (
 {
     fun findById (id: String): Mono<Question> {
         return template.findOne(query(where(Question::id).`is`(id)), "")
+    }
+
+    fun findRandom(category: String, size: Long): Flux<Question> {
+        val match = match(where(Question::cats).`is`(category))
+        val sample = sample(size)
+
+        val agg = newAggregation(Question::class.java, match, sample)
+        return template.aggregate(agg)
     }
 
 }
