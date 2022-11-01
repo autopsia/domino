03c49ed1d8f549de5f0b24613449533fca1025e3
autopsia <autopsia9@gmail.com>
2022-10-31 19:41:28 -0500

    Update dependencies and jvm target.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.platform.TargetPlatformVersion.NoVersion.description
 
 plugins {
-    id("org.springframework.boot") version "2.5.6"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.spring") version "1.5.30"
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 repositories {
@@ -19,16 +18,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     //springdoc
-    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
-    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.5.10")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.12")
     //kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.30")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.30")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     //testing
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testImplementation("io.projectreactor:reactor-test")
 
 }
@@ -36,7 +35,7 @@ dependencies {
 group = "com.sectordefectuoso"
 version = "0.0.2"
 description = "domino"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks.withType<JavaCompile>() {
     options.encoding = "UTF-8"
@@ -45,7 +44,7 @@ tasks.withType<JavaCompile>() {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
